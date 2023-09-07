f92fef6af7ec24a697eb31467aa37c9aebc3f392
Corey Light <coreylight@gmail.com>
2023-09-07 17:25:44 -0500

    yolo

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Trans } from '@lingui/macro'
+import { extra } from './extra'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log({ extra })
 
   return (
     <div>
